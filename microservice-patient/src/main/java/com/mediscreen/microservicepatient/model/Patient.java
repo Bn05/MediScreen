b363425c3c28e080aa1cc61f4fdde51b4edbab46
Bertrand <bertrandn5@gmail.com>
2023-03-21 15:59:24 +0100

    add eureka to docker compose

--- microservice-patient/src/main/java/com/mediscreen/microservicepatient/model/Patient.java
+++ microservice-patient/src/main/java/com/mediscreen/microservicepatient/model/Patient.java
@@ -26,7 +26,6 @@ public class Patient {
     @NotNull(message = "Merci d'indiquer votre date de naissance")
     private LocalDate birthdate;
     @NotBlank
-    @OneToMany(mappedBy="cart")
     private String gender;
     private String address;
     private String phoneNumber;
