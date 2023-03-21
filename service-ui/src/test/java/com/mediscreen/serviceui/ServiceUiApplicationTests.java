04fa889a81f1f3628eed09f915813582bfb16e7c
Bertrand <bertrandn5@gmail.com>
2023-03-21 16:00:37 +0100

    start service-ui

---
+++ service-ui/src/test/java/com/mediscreen/serviceui/ServiceUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.serviceui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
