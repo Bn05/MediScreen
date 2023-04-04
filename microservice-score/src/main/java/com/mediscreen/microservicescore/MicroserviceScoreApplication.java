d519805e2108d29cd088d8f49d24b2caf437be50
Bertrand <bertrandn5@gmail.com>
2023-04-04 10:49:01 +0200

    add microservice-score

---
+++ microservice-score/src/main/java/com/mediscreen/microservicescore/MicroserviceScoreApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservicescore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceScoreApplication.class, args);
+	}
+
+}
